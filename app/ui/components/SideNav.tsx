758bf370e0f4be821b77a6949f6df8e9cf1134ee
justinespinal <espinaljustin03@gmail.com>
2024-09-27 21:39:01 -0400

    Sidenav coloring fixes and dependency updates

--- app/ui/components/SideNav.tsx
+++ app/ui/components/SideNav.tsx
@@ -1,31 +1,57 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Link from "next/link"
+import clsx from "clsx"
+import { usePathname } from "next/navigation"
 
 import { faHouse, faUser, faStar, faTv } from "@fortawesome/free-solid-svg-icons"
 
 export default function SideNav(){
+    const pathname = usePathname()
+    console.log(pathname)
     return (
         <div className="flex lg:flex-col lg:mr-auto">
             <Link 
-              className="p-4 hover:bg-slate-500"
+              className={clsx(
+                "p-4 hover:bg-gray-700",
+                {
+                  "bg-[rgb(24,24,15)]":  pathname === '/home'
+                },
+              )}
               href="/home"
             >
                 <span><FontAwesomeIcon icon={faHouse} /></span>
             </Link>
             <Link
-              className="p-4 hover:bg-slate-500"
+              className={clsx(
+                "p-4 hover:bg-gray-700",
+                {
+                  "bg-[rgb(24,24,15)]":  pathname === '/home/about'
+                },
+              )}
               href="/home/about"
             >
                 <span><FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} /></span>
             </Link>
             <Link
-              className="p-4 hover:bg-slate-500"
+              className={clsx(
+                "p-4 hover:bg-gray-700",
+                {
+                  "bg-[rgb(24,24,15)]":  pathname === '/home/projects'
+                },
+              )}
               href="/home/projects"
             >
               <span><FontAwesomeIcon icon={faTv} style={{color: "#ffffff",}} /></span>
             </Link>
             <Link
-              className="p-4 hover:bg-slate-500"
+              className={clsx(
+                "p-4 hover:bg-gray-700",
+                {
+                  "bg-[rgb(24,24,15)]":  pathname === '/home/experience'
+                },
+              )}
               href="/home/experience"
             >
                 <span><FontAwesomeIcon icon={faStar} style={{color: "#ffffff",}} /></span>
